3bd78223657faab8259d79520d386239806ace43
Nathan Wood <nathan.ronald.wood@gmail.com>
2022-05-20 10:54:18 -0500

    Assignments completed. Moving on to clean it up

--- profile.js
+++ profile.js
@@ -21,3 +21,21 @@ ritualBtn.addEventListener("click", (event) => {
     "My favorite ritual is Eucharist, with video games in the morning being a close second."
   );
 });
+
+let recommendBtn = document.querySelector("#food");
+
+let restraunts = [
+  "Tako King's Palace",
+  "The Homeless Shelter",
+  "Taco Bell (inferior)",
+  "The Tako Cave",
+  "Takos for life",
+  "Inferior Tacos",
+];
+
+recommendBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  const randomElement =
+    restraunts[Math.floor(Math.random() * restraunts.length)];
+  alert(randomElement);
+});
